a423ebf8a8c1fb077d8843eaef44ce84efee1c52
WRXer <king_311@mail.ru>
2023-05-23 22:19:16 +0400

    сссс

--- main/models.py
+++ main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)    #наименование,
+    description = models.TextField()    #описание.
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = "Category"     # наименование модели в единственном числе
+        verbose_name_plural = "Categories"     # множественное число наименования модели
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)    #наименование
+    description = models.TextField()    #описание,
+    image = models.ImageField(upload_to='products/', **NULLABLE)    #изображение (превью),
+    category = models.ForeignKey(Category, on_delete=models.CASCADE())    #категория,
+    price = models.DecimalField(max_digits=10, decimal_places=3)     #цена за покупку,
+    created_at = models.DateTimeField(default=timezone.now)    #дата создания,
+    recreated_at = models.DateTimeField(auto_now=True)    #дата последнего изменения.
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = "Product"   # наименование модели в единственном числе
+        verbose_name_plural = "Products"    # множественное число наименования модели
