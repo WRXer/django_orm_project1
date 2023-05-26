ccd8a06b9e7df1e79f962e603507786ece6d058a
WRXer <king_311@mail.ru>
2023-05-26 16:03:34 +0400

    Для выводимого изображения на странице реализуйте шаблонный фильтр, который преобразует переданный путь в полный путь для доступа к медиафайлу

--- main/templatetags/my_tags.py
+++ main/templatetags/my_tags.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 register = template.Library()
 
-@register.filter
-def mediapath(value):
-    return f"{settings.MEDIA_URL}{value}"
+@register.simple_tag
+def mediapath(image_path):
+    full_image_path = settings.MEDIA_URL + str(image_path)
+    return full_image_path
